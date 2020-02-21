8f41baaef4cf99cb98133bed88d6bd994e8ebcd7
Shaolin P <shiulan.poe@outlook.com>
2020-02-20 23:26:26 -0500

    update

--- Program.cs
+++ Program.cs
@@ -5,51 +5,52 @@ namespace Assignment2_CT_Spring2020
     {
         static void Main(string[] args)
         {
-        Console.WriteLine("Question 1");
-        int[] l1 = new int[] { 5, 6, 6, 9, 9, 12 };
-        int target = 9;
-        int[] r = TargetRange(l1, target);
-        // Write your code to print range r here
-        Console.WriteLine("[{0}]", string.Join(", ", r));
-        Console.WriteLine();
-        //Console.WriteLine("Question 2");
-        //string s = "University of South Florida";
-        //string rs = StringReverse(s);
-        //Console.WriteLine(rs);
-        //Console.WriteLine("Question 3");
-        //int[] l2 = new int[] { 2, 2, 3, 5, 6 };
-        //int sum = MinimumSum(l2);
-        //Console.WriteLine(sum);
-        //Console.WriteLine("Question 4");
-        //string s2 = "Dell";
-        //string sortedString = FreqSort(s2);
-        //Console.WriteLine(sortedString);
-        //Console.WriteLine("Question 5-Part 1");
-        //int[] nums1 = { 1, 2, 2, 1 };
-        //int[] nums2 = { 2, 2 };
-        //int[] intersect1 = Intersect1(nums1, nums2);
-        //Console.WriteLine("Part 1- Intersection of two arrays is: ");
-        //DisplayArray(intersect1);
-        //Console.WriteLine("\n");
-        //Console.WriteLine("Question 5-Part 2");
-        //int[] intersect2 = Intersect2(nums1, nums2);
-        //Console.WriteLine("Part 2- Intersection of two arrays is: ");
-        //DisplayArray(intersect2);
-        //Console.WriteLine("\n");
-        //Console.WriteLine("Question 6");
-        //char[] arr = new char[] { 'a', 'g', 'h', 'a' };
-        //int k = 3;
-        //Console.WriteLine(ContainsDuplicate(arr, k));
-        //Console.WriteLine("Question 7");
-        //int rodLength = 4;
-        //int priceProduct = GoldRod(rodLength);
-        //Console.WriteLine(priceProduct);
-        //Console.WriteLine("Question 8");
-        //string[] userDict = new string[] { "rocky", "usf", "hello", "apple" };
-        //string keyword = "hhllo";
-        //Console.WriteLine(DictSearch(userDict, keyword));
-        //Console.WriteLine("Question 8");
-        //SolvePuzzle();
+            //Console.WriteLine("Question 1");
+            //int[] l1 = new int[] { 5, 6, 6, 9, 9, 12 };
+            //int target = 9;
+            //int[] r = TargetRange(l1, target);
+            //// Write your code to print range r here
+            //Console.WriteLine("[{0}]", string.Join(", ", r));
+            //Console.WriteLine();
+            Console.WriteLine("Question 2");
+            string s = "University of South Florida";
+            string rs = StringReverse(s);
+            Console.WriteLine(rs);
+            Console.WriteLine();
+            //Console.WriteLine("Question 3");
+            //int[] l2 = new int[] { 2, 2, 3, 5, 6 };
+            //int sum = MinimumSum(l2);
+            //Console.WriteLine(sum);
+            //Console.WriteLine("Question 4");
+            //string s2 = "Dell";
+            //string sortedString = FreqSort(s2);
+            //Console.WriteLine(sortedString);
+            //Console.WriteLine("Question 5-Part 1");
+            //int[] nums1 = { 1, 2, 2, 1 };
+            //int[] nums2 = { 2, 2 };
+            //int[] intersect1 = Intersect1(nums1, nums2);
+            //Console.WriteLine("Part 1- Intersection of two arrays is: ");
+            //DisplayArray(intersect1);
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Question 5-Part 2");
+            //int[] intersect2 = Intersect2(nums1, nums2);
+            //Console.WriteLine("Part 2- Intersection of two arrays is: ");
+            //DisplayArray(intersect2);
+            //Console.WriteLine("\n");
+            //Console.WriteLine("Question 6");
+            //char[] arr = new char[] { 'a', 'g', 'h', 'a' };
+            //int k = 3;
+            //Console.WriteLine(ContainsDuplicate(arr, k));
+            //Console.WriteLine("Question 7");
+            //int rodLength = 4;
+            //int priceProduct = GoldRod(rodLength);
+            //Console.WriteLine(priceProduct);
+            //Console.WriteLine("Question 8");
+            //string[] userDict = new string[] { "rocky", "usf", "hello", "apple" };
+            //string keyword = "hhllo";
+            //Console.WriteLine(DictSearch(userDict, keyword));
+            //Console.WriteLine("Question 8");
+            //SolvePuzzle();
         }
 
         public static int[] TargetRange(int[] l1, int t)
@@ -89,6 +90,41 @@ namespace Assignment2_CT_Spring2020
             
         }
 
+        public static string StringReverse(string s)
+        {
+            try
+            {
+                //write your code here
+                string[] arrayQ2 = s.Split(' ');
+                int sizeArrayQ2 = arrayQ2.Length;
+                string[] resortArrayQ2 = new string[sizeArrayQ2];
+                for (int countQ2 = 0; countQ2 < sizeArrayQ2; countQ2++)
+                {
+                    if (arrayQ2[countQ2] == " ")
+                    {
+                        resortArrayQ2[countQ2] = arrayQ2[countQ2];
+                    }
+                    else
+                    {
+                        char[] charArrayQ2 = arrayQ2[countQ2].ToCharArray();
+                        int sizeCharArrayQ2 = charArrayQ2.Length;
+                        char[] resortCharArrayQ2 = new char[sizeCharArrayQ2];
+                        for (int indexQ2 = 0; indexQ2 < sizeCharArrayQ2; indexQ2++)
+                        {
+                            resortCharArrayQ2[sizeCharArrayQ2 - 1 - indexQ2] = charArrayQ2[indexQ2];
+                        }
+                        resortArrayQ2[countQ2] = new string(resortCharArrayQ2);
+                    }
+                }
+                string resortWords = string.Join(" ", resortArrayQ2);
+                return resortWords;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
         public static void DisplayArray(int[] a)
         {
             foreach (int n in a)
@@ -97,18 +133,7 @@ namespace Assignment2_CT_Spring2020
             }
         }
         
-        public static string StringReverse(string s)
-        {
-            try
-            {
-                //write your code here
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-            return null;
-        }
+        
         public static int MinimumSum(int[] l2)
         {
             try
